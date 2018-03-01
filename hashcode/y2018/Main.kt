66506ba2600675459350ac62c9aa74961327a97e
Anton Bannykh <anton.bannykh@gmail.com>
2018-03-02 01:39:30 +0300

    Clean Main.kt up; leave our best solution for D and E

--- hashcode/y2018/Main.kt
+++ hashcode/y2018/Main.kt
@@ -11,77 +11,26 @@ val filename = "d_metropolis"
 fun main(args: Array<String>) {
     val sc = Scanner(File("data/$filename.in"))
 
-    val rows = sc.nextInt()
-    val columns = sc.nextInt()
-
-    val V = sc.nextInt()
-
-    val R = sc.nextInt()
-
+    val (_, _, V, R) = Array(4) { sc.nextInt() }
     B = sc.nextInt()
     val T = sc.nextInt()
 
     val rides = Array(R) { sc.nextRide(it) }
 
-    val origin = Point(0, 0)
+    val cars = Array(V) { Car(Point(0, 0), 0) }
 
-    val pos = Array(V) { Car(origin, 0, it) }
+    println("Max score: " + rides.sumBy { it.duration + B })
 
-    println("Max score: " + rides.sumBy { it.duration })
-
-//    val longRides = rides.filter { it.longRide }.sortedBy { it.duration }
-
-//    longRides.foldIndexed(0) { index, acc, ride ->
-//        println("$index $acc")
-//        acc + ride.duration
-//    }
-
-
-//    rides.sortBy { it.duration }
-//    for (i in rides.indices step 100) {
-//        println("$i ${rides[i].duration}")
-//    }
-//    rides.withIndex().forEach { (i, ride) ->  println("$i ${ride.duration}") }
-
-//    println("Long rides: ${rides.count { it.longRide }}")
-//    println("Long rides: ${rides.filter {it.longRide}.sumBy { it.duration }}")
-//    println("Short rides: ${rides.filter {!it.longRide}.sumBy { it.duration }}")
-
-    rides.sortBy { it.end - it.duration - it.duration / 2}
-
-//    rides.filter { it.longRide }.sortedByDescending { it.begin }.take(20).zip(pos).forEach {(ride, car) ->
-//        car.longRide = ride
-//    }
-
-//    println(rides.filter { !it.longRide }.maxBy { it.duration }?.duration)
+    rides.sortBy { it.end - it.duration }
 
     for (ride in rides) {
-//        if (ride.longRide) continue
-        val cars = pos//70.let { if (ride.longRide) pos.take(it) else pos.drop(it) }
-        var best: Car? = null
-        var bestScore = 0
-        var bestWait = Int.MAX_VALUE
-        for (p in cars) {
-            if (p.canScore(ride) > bestScore /*|| p.canScore(ride) == bestScore && p.waitTime(ride) < bestWait*/) {
-                best = p
-                bestScore = p.canScore(ride)
-                bestWait = p.waitTime(ride)
-            }
-        }
-
-        if (best != null) {
-            best.take(ride)
-        }
+        cars.maxBy { it.canScore(ride) }?.take(ride)
     }
 
-    println(pos.totalScore)
+    println(cars.totalScore)
 
-    printSolution(pos)
+    printSolution(cars)
 }
-
-val rand = Random()
-
-fun <T> Array<T>.shuffled() = toList().also { Collections.shuffle(it) }
 
 val Array<Car>.totalScore
     get() = fold(0) { acc, state -> acc + state.score }
@@ -90,10 +39,8 @@ var B = 0
 
 class Car(var point: Point,
           var time: Int,
-          val num: Int,
           val schedule: MutableList<Ride> = mutableListOf(),
-          var score: Int = 0,
-          var longRide: Ride? = null) {
+          var score: Int = 0) {
 
     fun willFinishAt(ride: Ride) = possibleArrival(ride) + ride.duration
 
@@ -102,16 +49,9 @@ class Car(var point: Point,
     fun waitTime(ride: Ride) = Math.max(possibleArrival(ride) - time, 0)
 
     fun canScore(ride: Ride): Int {
-        val possibleArrival = possibleArrival(ride)
-        if (possibleArrival + ride.duration <= ride.end) {
-            val lr = longRide
-            if (lr != null && ride !== lr && willFinishAt(ride) + (lr.start - ride.finish) + lr.duration > lr.end) {
-                return 0
-            }
-
-            return ride.duration + if (possibleArrival <= ride.begin) B else 0
-        }
-        return 0
+        return if (willFinishAt(ride) <= ride.end) {
+            ride.duration + if (possibleArrival(ride) <= ride.begin) B else 0
+        } else 0
     }
 
     fun take(ride: Ride) {
