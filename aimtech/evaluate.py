8bfe52199554ad8578ebc559acad804761997643
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2019-07-04 01:12:21 +0300

    Increase informativeness of evaluation script

--- aimtech/evaluate.py
+++ aimtech/evaluate.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 from res import aimmaze
+import math
+import sys
+
+
+def my_quit_ok(points, text):
+	_, exit_found, _, made_moves, _, made_ops, _, code_length, _, raw_score, _, maze_weight = text.split()
+	if exit_found != str(True):
+		text = 'Exit not found'
+	else:
+		brevity = round(100 * coefs[1] / sum(coefs))
+		parts = [int(maze_weight) * c for c in coefs]
+		text = f'OUR={points:.3f}={parts[0]:.0f}+{parts[1]:.0f}\tMOVES={made_moves}\tOPS={made_ops}\tCODE={code_length}\tBREV={brevity}%'
+	print(text, file=sys.stderr)
+	sys.exit(7)
+
+def my_log(x):
+	coefs.append(saved_log(x))
+	return saved_log(x)
+
+saved_log = math.log2
+coefs = []
 
 def main():
 	aimmaze.CELL_SIZE = 4
@@ -9,17 +30,8 @@ def main():
 	aimmaze.WALL_COLOR = "gray"
 	aimmaze.GRID_COLOR = "white"
 	aimmaze.KEY_COLOR = "red"
-
-	their_quit_ok = aimmaze.quit_ok
-
-	def my_quit_ok(points, text):
-	    #assert(points >= 0 and points <= 1e18)
-	    #print("points {:.4f} {}".format(points, text), file=sys.stderr)
-	    #sys.exit(7)
-	    their_quit_ok(points, text)
-
 	aimmaze.quit_ok = my_quit_ok
-
+	math.log2 = my_log
 	aimmaze.main()
 
 
